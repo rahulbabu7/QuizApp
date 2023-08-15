d4e1fdce883b8490bbc611ff8c03557700fec48f
rahulbabu7 <rahulbabu436@gmail.com>
2023-08-15 22:42:17 +0530

    added searc quiz feature

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,35 +1,49 @@
 <script setup>
-import q from '../data/Quiz.json'   //importing jsoin file 
-import {ref} from 'vue' 
-const quizes = ref(q); //converting the q into a state for search feature  we can for loop through this array
+import ques from '../data/Quiz.json'   //importing jsoin file 
+import { ref,watch } from 'vue'
+const quizes = ref(ques); //converting the q into a state for search feature  we can for loop through this array
+const search = ref("");
+//const quizes = q;  even without ref we can loop through json using this line . But ref is provided to display cards based on the search results
+watch(search ,()=>{
+    //watch is used to continuosly listen to the search .
+    //whenever the searcg changes it state we should updates the quizes state also.
+
+    //logic
+    quizes.value = ques.filter(quiz =>
+        quiz.name.toLowerCase().includes(search.value.toLowerCase())
+        //filters the cards that has the value related to the search .
+        //quiz is a looping variable in the array ques
+        //the quiz name should include the letters from the search
+    )
+
+})
 </script>
 <template>
-
-<div class="container">
-    <header>
-        <h1> Quizes</h1>
-        <input type="text" placeholder="search">
-    </header>
-    <main>
-     <section class="card" v-for="quiz in quizes" 
-     :key="quiz.id">
-        <img :src="quiz.img" alt=" MATHS">
-        <section class="card-text">
-            <h2>{{quiz.name}}</h2>
-        <p>{{ quiz.questions.length}}</p>
-        </section>
-     </section>
-    </main>
-</div>
+    <div class="container">
+        <header>
+            <h1> Quizes</h1>
+            <input type="text" placeholder="search" v-model.trim="search">
+        </header>
+        <main>
+            <section class="card" v-for="quiz in quizes" :key="quiz.id">
+                <img :src="quiz.img" alt=" MATHS">
+                <section class="card-text">
+                    <h2>{{ quiz.name }}</h2>
+                    <p>{{ quiz.questions.length }}</p>
+                </section>
+            </section>
+        </main>
+    </div>
 </template>
 
 
 <style scoped>
-.container{
+.container {
     max-width: 1000px;
     margin: 0 auto;
 }
-header{
+
+header {
     margin-bottom: 10px;
     margin-top: 30px;
     display: flex;
@@ -37,40 +51,46 @@ header{
     gap: 50px;
 
 }
-h1{
+
+h1 {
     font-weight: bold;
     margin-right: 30px;
 }
-input{
+
+input {
     border: none;
-    background-color: rgba(128,128 , 128, 0.1);
+    background-color: rgba(128, 128, 128, 0.1);
     padding: 10px;
     border-radius: 5px;
 }
-main{
+
+main {
     display: flex;
     flex-wrap: wrap;
     margin-top: 40px;
 }
-.card{
+
+.card {
     width: 310px;
     overflow: hidden;
     border-radius: 2%;
-    box-shadow: 1px 1px 10px rgba(0,0, 0, 0.1);
+    box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
     margin-bottom: 35px;
     margin-right: 20px;
     cursor: pointer;
 }
-.card img{
+
+.card img {
     width: 100%;
     height: 190px;
     margin: 0;
 }
-.card-text{
+
+.card-text {
     padding: 0 5px;
 
 }
-.card-text h2{
+
+.card-text h2 {
     font-weight: bold;
-}
-</style>
+}</style>
