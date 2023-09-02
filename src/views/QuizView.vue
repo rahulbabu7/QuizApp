a11b8905060f53cb79039a140c35bf33a6bdf809
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-02 11:29:19 +0530

    added semantic

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,39 +1,37 @@
-<script scoped>
-
-
-</script>
-
+<script></script>
 <template>
-    <main>
-        <header>
-        <h1> Question 1/3</h1>
+  <header>
+    <h1>Question 1/3</h1>
     <div class="bar">
         <div class="completion">
 
         </div>
     </div>
-    </header>
-    </main>
-  
-    
+  </header>
+  <main>
+    <section>
+      <h2 class="question">What is the chemical name of table salt</h2>
+      <aside class="answers">
+        
+      </aside>
+    </section>
+  </main>
 </template>
-
-<style scoped>
-header{
-    margin:top 20px;
+<style>
+header {
+  margin: top 20px;
 }
-h1{
-    font-size: 30px;
+h1 {
+  font-size: 30px;
 }
-.bar{
-    width: 300px;
-    height: 50px;
-    border: 3px solid rebeccapurple;
+.bar {
+  width: 300px;
+  height: 50px;
+  border: 3px solid rebeccapurple;
 }
-.completion{
-    height: 100%;
-    width: 0%;
-    background-color: rebeccapurple;
+.completion {
+  height: 100%;
+  width: 0%;
+  background-color: rebeccapurple;
 }
-
 </style>
