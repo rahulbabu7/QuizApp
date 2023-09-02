c41ac6c167fa2ad81b993c702087fed3d6d2c9ac
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-02 11:50:36 +0530

    updated the components

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,34 +1,16 @@
 <script setup>
 
-import Question from '../components/Question.vue' 
+import Question from '../components/Question.vue' ;
+import QuizHeader from '../components/QuizHeader.vue';
 </script>
+
 <template>
-  <header>
-    <h1>Question 1/3</h1>
-    <div class="bar">
-      <div class="completion"></div>
-    </div>
-  </header>
+  <QuizHeader />
   <main>
 <Question />
   </main>
 </template>
-<style>
-header {
-  margin: top 20px;
-}
-h1 {
-  font-size: 30px;
-}
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid rebeccapurple;
-}
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: rebeccapurple;
-}
+
+<style scoped>
 
 </style>
