89eabcacf69a12299b777b782cf4b8cb7ee5d5fd
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-02 11:59:51 +0530

    updating ques index

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -3,18 +3,18 @@
 import Question from '../components/Question.vue' ;
 import QuizHeader from '../components/QuizHeader.vue';
 import { useRoute} from 'vue-router';
+import { ref } from 'vue';
 const route = useRoute();
 // console.log(route.params.id) returns the id of the router
 const quizId = parseInt(route.params.id);
+const currentQuesIndex = ref(0);
 </script>
 
 <template>
   <QuizHeader />
   <main>
-<Question />
+    <Question />
   </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
