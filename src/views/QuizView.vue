df8916ebb8e793148e5147b0bcc16090464d82e5
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-02 11:56:08 +0530

    working on question

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,6 +2,10 @@
 
 import Question from '../components/Question.vue' ;
 import QuizHeader from '../components/QuizHeader.vue';
+import { useRoute} from 'vue-router';
+const route = useRoute();
+// console.log(route.params.id) returns the id of the router
+const quizId = parseInt(route.params.id);
 </script>
 
 <template>
